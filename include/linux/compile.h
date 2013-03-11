4fb6d20b6bbd134551b525da7d2851dd0f39b586
Blechd0se <alex.christ@hotmail.de>
2013-03-11 20:12:51 +0100

    Add untracked files

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 14 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 PREEMPT Mon Mar 11 17:14:41 CET 2013"
+#define LINUX_COMPILE_TIME "17:14:41"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.5.4 20110526 (prerelease) (Linaro GCC 4.5-2011.06-0) "
